70d18d9875230e7fce6bf5bc6f7b44d8f5193863
Côme Périn <come.perin@bordeaux-inp.fr>
2024-10-24 09:14:56 +0200

    make_profile(s) is ok

--- src/resolve/Progress.py
+++ src/resolve/Progress.py
@@ -1,3 +1,5 @@
+from bitarray import bitarray
+
 from ..Graph import Graph
 from ..Station import Station
 from ..Olympic import Olympic
@@ -7,6 +9,7 @@ class Progress:
     def solve(G:Graph):
         profiles = Progress.make_profiles(G)
 
+    #returns an array of bitarray
     def make_profiles(G:Graph):
         stations = G.getStations()
         olympics = G.getOlympics()
@@ -17,9 +20,12 @@ class Progress:
             profiles.append(profile)
         return profiles
     
+    #returns a bitarray
     def make_profile(G:Graph, s:Station, olympics: list[Olympic]):
-        profile = 0b0
+        profile = bitarray(len(olympics))
+        profile.setall(0)
         for o in olympics:
             if G.are_adjacent(s, 0):
-                profile = profile | 0b1
-            #
+                profile[o]=1
+        return profile
+        
